c6fc04f62b077298bcb3019bafa56a22f59628a2
deepa-kakade-git <deep_303@yahoo.com>
2024-02-29 13:00:26 -0500

    I made some changes

--- object1/src/main/java/object1/Main.java
+++ object1/src/main/java/object1/Main.java
@@ -6,5 +6,6 @@ public class Main {
 
     public static void main(String... args) {
         Logger logger=Logger.getLogger(Main.class.getName());
-        logger.info("This is a module-using Hello World!");    }
+        logger.info("This is a module-using Hello World!");
+    }
 }
---
+++ object1/src/main/java/object1/chair.java
@@ -0,0 +1,13 @@
+package object1;
+
+public class chair {
+    private String material;
+    private String color;
+    private int legs;
+
+    public Chair(String material , String color, int legs){
+        this.material = material;
+        this.color = color;
+        
+    }
+}
